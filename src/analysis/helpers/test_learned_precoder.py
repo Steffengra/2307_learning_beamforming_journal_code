d7692ab42bb42343c52daf8c69c584c49daeeab8
S <stgr+github@posteo.net>
2024-05-26 14:21:07 +0200

    added test learned rsma complete

--- src/analysis/helpers/test_learned_precoder.py
+++ src/analysis/helpers/test_learned_precoder.py
@@ -7,6 +7,7 @@ import src
 from src.analysis.helpers.test_precoder_error_sweep import test_precoder_error_sweep
 from src.analysis.helpers.test_precoder_user_distance_sweep import test_precoder_user_distance_sweep
 from src.data.calc_sum_rate import calc_sum_rate
+from src.data.calc_sum_rate_RSMA import calc_sum_rate_RSMA
 from src.utils.load_model import (
     load_model,
     load_models,
@@ -16,6 +17,7 @@ from src.utils.get_precoding import (
     get_precoding_learned_decentralized,
     get_precoding_adapted_slnr_powerscaled,
     get_precoding_adapted_slnr_complete,
+    get_precoding_learned_rsma_complete,
 )
 
 
@@ -123,4 +125,25 @@ def test_adapted_slnr_powerscaled_error_sweep(
         monte_carlo_iterations=monte_carlo_iterations,
         get_precoder_func=lambda cfg, sat_man: get_precoding_adapted_slnr_powerscaled(cfg, sat_man, norm_factors, scaling_network),
         calc_sum_rate_func=calc_sum_rate,
+    )
+
+
+def test_learned_rsma_complete(
+        config: 'src.config.config.Config',
+        model_path: Path,
+        error_sweep_parameter: str,
+        error_sweep_range: np.ndarray,
+        monte_carlo_iterations: int,
+) -> None:
+
+    rsma_network, norm_factors = load_model(model_path)
+
+    test_precoder_error_sweep(
+        config=config,
+        error_sweep_parameter=error_sweep_parameter,
+        error_sweep_range=error_sweep_range,
+        precoder_name='learned_rsma_full',
+        monte_carlo_iterations=monte_carlo_iterations,
+        get_precoder_func=lambda cfg, sat_man: get_precoding_learned_rsma_complete(cfg, sat_man, norm_factors, rsma_network),
+        calc_sum_rate_func=calc_sum_rate_RSMA,
     )
