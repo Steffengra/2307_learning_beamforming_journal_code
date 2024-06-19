431df24fc83e1564c6cc9a1284a897b8f6af26be
S <stgr+github@posteo.net>
2024-06-19 15:01:24 +0200

    added journal sweeps

---
+++ src/models/journal_training_sweeps/train_sac_sweep_1sat.py
@@ -0,0 +1,201 @@
+
+from pathlib import Path
+from sys import path as sys_path
+project_root_path = Path(Path(__file__).parent, '../..', '..')
+sys_path.append(str(project_root_path.resolve()))
+
+import numpy as np
+from copy import deepcopy
+
+from src.config.config import Config
+from src.models.train_sac import train_sac
+from src.models.train_sac_adapt_robust_slnr_power import train_sac_adapt_robust_slnr_power
+from src.models.train_sac_adapt_robust_slnr_complete import train_sac_adapt_robust_slnr_complete
+from src.analysis.helpers.test_mmse_precoder import test_mmse_precoder_error_sweep
+from src.analysis.helpers.test_robust_slnr_precoder import test_robust_slnr_precoder_error_sweep
+from src.analysis.helpers.test_learned_precoder import (
+    test_sac_precoder_error_sweep,
+    test_adapted_slnr_powerscaled_error_sweep,
+    test_adapted_slnr_complete_error_sweep,
+)
+
+
+def learn_baseline(
+        user_dist: float,
+        user_wiggle: float,
+        additive_error_on_cosine_of_aod: float,
+        testing_range: np.ndarray,
+) -> None:
+
+    config = Config()
+    assert config.sat_nr == 1
+    assert config.sat_tot_ant_nr == 16
+    assert config.user_nr == 3
+    assert config.config_error_model.error_rng_parametrizations['large_scale_fading']['args']['sigma'] == 0.1
+
+    config.profile = False
+    config.show_plots = False
+    config.verbosity = 0
+
+    config.user_dist_average = user_dist
+    config.user_dist_bound = user_wiggle
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['low'] = -additive_error_on_cosine_of_aod
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high'] = additive_error_on_cosine_of_aod
+
+    config_name = config.generate_name_from_config()
+    config_name += f'_additive_{additive_error_on_cosine_of_aod}'
+    config.config_learner.training_name = config_name
+
+    best_model_path, _ = train_sac(config=config)
+
+    test_sac_precoder_error_sweep(
+        config=deepcopy(config),
+        model_path=best_model_path,
+        error_sweep_range=testing_range,
+        monte_carlo_iterations=5_000,
+        error_sweep_parameter='additive_error_on_cosine_of_aod',
+    )
+
+    if additive_error_on_cosine_of_aod == 0.0:
+        test_mmse_precoder_error_sweep(
+            config=deepcopy(config),
+            error_sweep_parameter='additive_error_on_cosine_of_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+        test_robust_slnr_precoder_error_sweep(
+            config=deepcopy(config),
+            error_sweep_parameter='additive_error_on_cosine_of_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+
+def learn_other_error_model(
+        additive_error_on_cosine_of_aod,
+        additive_error_on_aod,
+        testing_range,
+) -> None:
+
+    config = Config()
+    assert config.sat_nr == 1
+    assert config.sat_tot_ant_nr == 16
+    assert config.user_nr == 3
+    assert config.config_error_model.error_rng_parametrizations['large_scale_fading']['args']['sigma'] == 0.1
+
+    config.profile = False
+    config.show_plots = False
+    config.verbosity = 0
+
+    config.user_dist_average = 100_000
+    config.user_dist_bound = 50_000
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['low'] = -additive_error_on_cosine_of_aod
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high'] = additive_error_on_cosine_of_aod
+    config.config_error_model.error_rng_parametrizations['additive_error_on_aod']['args']['scale'] = additive_error_on_aod
+
+    config_name = config.generate_name_from_config()
+    config_name += f'_additive_{additive_error_on_cosine_of_aod}_{additive_error_on_aod}'
+    config.config_learner.training_name = config_name
+
+    print('training sac')
+    best_model_path, _ = train_sac(config=config)
+    print(f'best model path {best_model_path}')
+
+    print('testing sac')
+    test_sac_precoder_error_sweep(
+        config=deepcopy(config),
+        model_path=best_model_path,
+        error_sweep_range=testing_range,
+        monte_carlo_iterations=5_000,
+        error_sweep_parameter='additive_error_on_aod',
+    )
+
+    if additive_error_on_cosine_of_aod == 0.0:
+        print('testing mmse')
+        test_mmse_precoder_error_sweep(
+            config=deepcopy(config),
+            error_sweep_parameter='additive_error_on_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+        print('testing slnr')
+        test_robust_slnr_precoder_error_sweep(
+            config=deepcopy(config),
+            error_sweep_parameter='additive_error_on_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+
+def learn_adapt_slnr(
+        user_dist,
+        user_wiggle,
+        additive_error_on_cosine_of_aod,
+        testing_range,
+) -> None:
+
+    config = Config()
+    assert config.sat_nr == 1
+    assert config.sat_tot_ant_nr == 16
+    assert config.user_nr == 3
+    assert config.config_error_model.error_rng_parametrizations['large_scale_fading']['args']['sigma'] == 0.1
+
+    config.profile = False
+    config.show_plots = False
+    config.verbosity = 0
+
+    config.user_dist_average = user_dist
+    config.user_dist_bound = user_wiggle
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['low'] = -additive_error_on_cosine_of_aod
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high'] = additive_error_on_cosine_of_aod
+
+    config_name = config.generate_name_from_config()
+    config_name += f'_additive_{additive_error_on_cosine_of_aod}'
+    config.config_learner.training_name = config_name
+
+    print('training adapt')
+    best_model_path_adapt_full, _ = train_sac_adapt_robust_slnr_complete(config=config)
+    best_model_path_adapt_power, _ = train_sac_adapt_robust_slnr_power(config=config)
+
+    print('testing adapt')
+    test_adapted_slnr_complete_error_sweep(
+        config=config,
+        model_path=best_model_path_adapt_full,
+        error_sweep_parameter='additive_error_on_cosine_of_aod',
+        error_sweep_range=testing_range,
+        monte_carlo_iterations=5_000,
+    )
+    test_adapted_slnr_powerscaled_error_sweep(
+        config=config,
+        model_path=best_model_path_adapt_power,
+        error_sweep_parameter='additive_error_on_cosine_of_aod',
+        error_sweep_range=testing_range,
+        monte_carlo_iterations=5_000,
+    )
+
+
+def main():
+
+    testing_error_sweep_range_100k = np.linspace(0, 0.1, 9)
+    testing_error_sweep_range_10k = np.linspace(0, 0.12, 9)
+    testing_error_sweep_range_100k_error2 = np.linspace(0, 0.1, 9)
+
+    learn_baseline(user_dist=100_000, user_wiggle=50_000, additive_error_on_cosine_of_aod=0.0, testing_range=testing_error_sweep_range_100k)
+    learn_baseline(user_dist=100_000, user_wiggle=50_000, additive_error_on_cosine_of_aod=0.025, testing_range=testing_error_sweep_range_100k)
+    learn_baseline(user_dist=100_000, user_wiggle=50_000, additive_error_on_cosine_of_aod=0.05, testing_range=testing_error_sweep_range_100k)
+
+    learn_baseline(user_dist=10_000, user_wiggle=5_000, additive_error_on_cosine_of_aod=0.00, testing_range=testing_error_sweep_range_10k)
+    learn_baseline(user_dist=10_000, user_wiggle=5_000, additive_error_on_cosine_of_aod=0.03, testing_range=testing_error_sweep_range_10k)
+
+    learn_other_error_model(additive_error_on_cosine_of_aod=0.1, additive_error_on_aod=0.0, testing_range=testing_error_sweep_range_100k_error2)
+    learn_other_error_model(additive_error_on_cosine_of_aod=0.1, additive_error_on_aod=0.025, testing_range=testing_error_sweep_range_100k_error2)
+    learn_other_error_model(additive_error_on_cosine_of_aod=0.1, additive_error_on_aod=0.05, testing_range=testing_error_sweep_range_100k_error2)
+
+    learn_adapt_slnr(user_dist=10_000, user_wiggle=5_000, additive_error_on_cosine_of_aod=0.00, testing_range=testing_error_sweep_range_10k)
+    learn_adapt_slnr(user_dist=10_000, user_wiggle=5_000, additive_error_on_cosine_of_aod=0.03, testing_range=testing_error_sweep_range_10k)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/models/journal_training_sweeps/train_sac_sweep_2sat.py
@@ -0,0 +1,177 @@
+
+from pathlib import Path
+from sys import path as sys_path
+project_root_path = Path(Path(__file__).parent, '../..', '..')
+sys_path.append(str(project_root_path.resolve()))
+
+import numpy as np
+from copy import deepcopy
+
+from src.config.config import Config
+from src.models.helpers.get_state import get_state_erroneous_channel_state_information_local
+from src.models.train_sac import train_sac
+from src.models.train_sac_decentralized_blind import train_sac_decentralized_blind
+from src.models.train_sac_decentralized_limited import train_sac_decentralized_limited
+from src.analysis.helpers.test_mmse_precoder import test_mmse_precoder_error_sweep
+from src.analysis.helpers.test_robust_slnr_precoder import test_robust_slnr_precoder_error_sweep
+from src.analysis.helpers.test_learned_precoder import (
+    test_sac_precoder_error_sweep,
+    test_sac_precoder_decentralized_blind_error_sweep,
+    test_sac_precoder_decentralized_limited_error_sweep,
+)
+
+
+def learn_baseline(
+        sat_dist: float,
+        user_dist: float,
+        user_wiggle: float,
+        additive_error_on_cosine_of_aod: float,
+        testing_range: np.ndarray,
+) -> None:
+
+    config = Config()
+    assert config.sat_nr == 2
+    assert config.sat_tot_ant_nr == 4
+    assert config.user_nr == 3
+    assert config.sat_dist_bound == 0
+    assert config.config_error_model.error_rng_parametrizations['large_scale_fading']['args']['sigma'] == 0.1
+
+    config.profile = False
+    config.show_plots = False
+    config.verbosity = 0
+
+    config.sat_dist_average = sat_dist
+    config.user_dist_average = user_dist
+    config.user_dist_bound = user_wiggle
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['low'] = -additive_error_on_cosine_of_aod
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high'] = additive_error_on_cosine_of_aod
+
+    config_name = config.generate_name_from_config()
+    config_name += f'_additive_{additive_error_on_cosine_of_aod}'
+    config.config_learner.training_name = config_name
+
+    print('training sac')
+    best_model_path, _ = train_sac(config=config)
+    print(f'best model path {best_model_path}')
+
+    print('testing sac')
+    test_sac_precoder_error_sweep(
+        config=deepcopy(config),
+        model_path=best_model_path,
+        error_sweep_range=testing_range,
+        monte_carlo_iterations=5_000,
+        error_sweep_parameter='additive_error_on_cosine_of_aod',
+    )
+
+    if additive_error_on_cosine_of_aod == 0.0:
+        print('testing mmse')
+        test_mmse_precoder_error_sweep(
+            config=deepcopy(config),
+            error_sweep_parameter='additive_error_on_cosine_of_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+
+def learn_decentralized(
+        sat_dist: float,
+        user_dist: float,
+        user_wiggle: float,
+        additive_error_on_cosine_of_aod: float,
+        mode: str,
+        testing_range: np.ndarray,
+) -> None:
+
+    config = Config()
+    assert config.sat_nr == 2
+    assert config.sat_tot_ant_nr == 4
+    assert config.user_nr == 3
+    assert config.sat_dist_bound == 0
+    assert config.config_error_model.error_rng_parametrizations['large_scale_fading']['args']['sigma'] == 0.1
+
+    config.profile = False
+    config.show_plots = False
+    config.verbosity = 0
+
+    config.sat_dist_average = sat_dist
+    config.user_dist_average = user_dist
+    config.user_dist_bound = user_wiggle
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['low'] = -additive_error_on_cosine_of_aod
+    config.config_error_model.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high'] = additive_error_on_cosine_of_aod
+
+    config_name = config.generate_name_from_config()
+    config_name += f'_additive_{additive_error_on_cosine_of_aod}'
+    config.config_learner.training_name = config_name
+
+    print('training sac')
+    if mode == 'blind':
+        best_model_path, _ = train_sac_decentralized_blind(config=config)
+        print(f'best model path {best_model_path}')
+
+        test_sac_precoder_decentralized_blind_error_sweep(
+            config=deepcopy(config),
+            models_path=best_model_path,
+            error_sweep_parameter='additive_error_on_cosine_of_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+    elif mode == 'L1':
+        config.local_csi_own_quality = 'error_free'
+        config.local_csi_others_quality = 'erroneous'
+        # config.config_learner.get_state = get_state_erroneous_channel_state_information_local
+        # config.config_learner.get_state_args['local_csi_own_quality'] = config.local_csi_own_quality
+        # config.config_learner.get_state_args['local_csi_others_quality'] = config.local_csi_others_quality
+        best_model_path, _ = train_sac_decentralized_limited(config=config)
+        print(f'best model path {best_model_path}')
+
+        test_sac_precoder_decentralized_limited_error_sweep(
+            config=deepcopy(config),
+            models_path=best_model_path,
+            error_sweep_parameter='additive_error_on_cosine_of_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+    elif mode == 'L2':
+        config.local_csi_own_quality = 'erroneous'
+        config.local_csi_others_quality = 'scaled_erroneous'
+        # config.config_learner.get_state = get_state_erroneous_channel_state_information_local
+        # config.config_learner.get_state_args['local_csi_own_quality'] = config.local_csi_own_quality
+        # config.config_learner.get_state_args['local_csi_others_quality'] = config.local_csi_others_quality
+        best_model_path, _ = train_sac_decentralized_limited(config=config)
+        print(f'best model path {best_model_path}')
+
+        test_sac_precoder_decentralized_limited_error_sweep(
+            config=deepcopy(config),
+            models_path=best_model_path,
+            error_sweep_parameter='additive_error_on_cosine_of_aod',
+            error_sweep_range=testing_range,
+            monte_carlo_iterations=5_000,
+        )
+
+    else:
+        raise ValueError(f'Unknown mode {mode}')
+
+
+def main():
+
+    testing_error_sweep_range_100k = np.linspace(0, 0.5, 9)
+    testing_error_sweep_range_10k = np.linspace(0, 0.5, 9)
+
+    learn_baseline(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.0, testing_range=testing_error_sweep_range_100k)
+    learn_baseline(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.125, testing_range=testing_error_sweep_range_100k)
+    learn_baseline(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.25, testing_range=testing_error_sweep_range_100k)
+
+    learn_baseline(sat_dist=10_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.0, testing_range=testing_error_sweep_range_10k)
+    learn_baseline(sat_dist=10_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.125, testing_range=testing_error_sweep_range_10k)
+    learn_baseline(sat_dist=10_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.25, testing_range=testing_error_sweep_range_10k)
+
+    learn_decentralized(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.0, mode='blind', testing_range=testing_error_sweep_range_100k)
+    learn_decentralized(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.25, mode='blind', testing_range=testing_error_sweep_range_100k)
+    learn_decentralized(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.25, mode='L1', testing_range=testing_error_sweep_range_100k)
+    learn_decentralized(sat_dist=100_000, user_dist=1_000, user_wiggle=500, additive_error_on_cosine_of_aod=0.25, mode='L2', testing_range=testing_error_sweep_range_100k)
+
+
+if __name__ == '__main__':
+    main()
